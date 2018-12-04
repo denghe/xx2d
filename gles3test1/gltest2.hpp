633b6cdb83d2bb9036b097b8e6d426c563b89a39
denghe <denghe@hotmail.com>
2018-12-04 21:13:35 +0800

    writing

--- gles3test1/gltest2.hpp
+++ gles3test1/gltest2.hpp
@@ -210,24 +210,32 @@ struct GLTest2
 {
 	inline static var vsSrc = R"--(
 #version 300 es
-layout(location = 0) in vec4 vPosition;
+layout(location = 0) in vec4 aColor;
+layout(location = 1) in vec4 aPosition;
+out vec4 vColor;
 void main()
 {
-   gl_Position = vPosition;
+   vColor = aColor;
+   gl_Position = aPosition;
 }
 )--";
 
 	inline static var fsSrc = R"--(
 #version 300 es
 precision mediump float;
-out vec4 fragColor;
+in vec4 vColor;
+out vec4 oFragColor;
 void main()
 {
-   fragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );
+   oFragColor = vColor;
 }
 )--";
 
-	inline static GLfloat verts[] =
+	inline static std::array<GLfloat, 4> color = 
+	{
+		1.0f, 0.0f, 0.0f, 1.0f
+	};
+	inline static std::array<GLfloat, 9> verts =
 	{
 		0.0f,   0.5f,  0.0f,
 		-0.5f, -0.5f,  0.0f,
@@ -253,7 +261,7 @@ void main()
 		assert(program);
 
 		// 设置清屏颜色
-		glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	}
 
 	inline void Update(int const& width, int const& height, float time) noexcept
@@ -279,17 +287,23 @@ void main()
 			return;
 		}
 
-		// todo: 根据变量名来得到属性下标, 取代下面用到的 0
+		// 用数组内容填充属性 location = 0 处长度为 4 的 float 值
+		// 参数表: GLuint index, GLfloat* v
+		// index: 与 shader 代码里的 layout(location 值 相对应
+		// v: 传入数据所在数组
+		// 出错可能: index 超范围. 这里写死的故不需要判断了
+		glVertexAttrib4fv(0, color.data());
+		
 
 		// 改上下文中的 program 的 通用顶点属性数
 		// 参数表: GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride,	const GLvoid * pointer
-		// index: 0, 与 shader 代码里的 layout(location = 0) 相对应
-		// size: 3, 与 verts 数组相对应. 虽然 in vec4 vPosition 长度是 4, 但是这样可以填充 vPosition 的前 3 个属性: x, y, z
-		// type: GL_FLOAT, 表明 verts 数组 的成员的数据类型为 float
-		// normalized: GL_FALSE, specifies whether fixed-point data values should be normalized (GL_TRUE) or converted directly as fixed-point values (GL_FALSE) when they are accessed. This parameter is ignored if type is GL_FIXED.
+		// index: 与 shader 代码里的 layout(location 值 相对应
+		// size: 与 verts 数组相对应. 因为属性是 vec4, 容器长度是 4, 但数据长度是 3, 故填充为: x, y, z, 1.0f
+		// type: 属性基本元素数据类型
+		// normalized: false: 直接转为 float. true: float 类的保持不变, 符号整型映射到 -1 ~ 1, 无符整型映射到 0 ~ 1 区间
 		// stride: 0, 数据跨距为 0, 与 verts 数组里全是顶点数据的情况相符. 
 		// pointer: verts, 告之数据指针
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, verts);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, verts.data());
 		if (var e = glGetError())
 		{
 			lastErrorMessage.append("glVertexAttribPointer glGetError() = ");
@@ -298,19 +312,20 @@ void main()
 		}
 
 		// 启用顶点属性数组
-		glEnableVertexAttribArray(0);
-		if (var e = glGetError())
-		{
-			lastErrorMessage.append("glEnableVertexAttribArray glGetError() = ");
-			lastErrorMessage.append(std::to_string(glGetError()));
-			return;
-		}
+		// 出错可能: index 超范围. 这里写死的故不需要判断了
+		glEnableVertexAttribArray(1);
 
 		// 使用数组数据绘制图形. 
 		// 参数表: GLenum mode, GLint first, GLsizei count
 		// mode: GL_POINTS, GL_LINE_STRIP, GL_LINE_LOOP, GL_LINES, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN, GL_TRIANGLES
 		// first: 指定数组中的数据的起始下标. 实际数据偏移量为 first * ((顶点数据个数 + 跨距个数) * sizeof(数据类型))
 		// count: 指定要绘制多少个顶点
-		glDrawArrays(GL_LINE_LOOP, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		if (var e = glGetError())
+		{
+			lastErrorMessage.append("glDrawArrays glGetError() = ");
+			lastErrorMessage.append(std::to_string(glGetError()));
+			return;
+		}
 	}
 };
