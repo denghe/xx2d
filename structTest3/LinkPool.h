9ed2cc50199cbb2a991b05e4aaa6f849dcbdce97
denghe <denghe@hotmail.com>
2021-01-24 21:07:58 +0800

    writing...

---
+++ structTest3/LinkPool.h
@@ -0,0 +1,98 @@
+﻿#include <cstring>	// malloc realloc
+//#include <cstddef>	// offsetof
+#include <cassert>
+#include <type_traits>
+
+// 用来标识 LinkPool.buf 的成员类型 是否支持 realloc 扩容
+template<typename T, typename ENABLED = void> struct IsReallocable : std::false_type {};
+template<typename T> struct IsReallocable<T, std::enable_if_t<std::is_standard_layout_v<T> && std::is_trivial_v<T>>> : std::true_type {};
+template<typename T> constexpr bool IsReallocable_v = IsReallocable<T>::value;
+
+
+// 链表节点池. 分配的是不会变化的 下标. 可指定 next 变量类型和偏移量
+template<typename T, typename PNType = int, size_t nextOffset = 0>
+class LinkPool {
+	static_assert(sizeof(T) >= sizeof(int));
+	static_assert(sizeof(T) >= sizeof(int) + nextOffset);
+
+	T* buf = nullptr;			// 数组
+	PNType len = 0;				// 已使用（并非已分配）对象个数
+	PNType cap = 0;				// 数组长度
+	PNType header = -1;			// 未分配节点单链表头下标
+	PNType count = 0;			// 未分配节点单链表长度
+
+	PNType& Next(T& o) {
+		return *(PNType*)((char*)&o + nextOffset);
+	}
+public:
+	explicit LinkPool(PNType const& cap = 16) : cap(cap) {
+		buf = (T*)malloc(sizeof(T) * cap);
+	}
+	~LinkPool() {
+		Clear();
+		free((void*)buf);
+		buf = nullptr;
+	}
+
+	// 分配一个下标到 idx. 如果产生了 resize 行为，将返回新的 cap
+	PNType Alloc(PNType& idx) {
+		if (count) {
+			idx = header;
+			header = Next(buf[idx]);
+			--count;
+			return 0;
+		}
+		else {
+			if (len == cap) {
+				cap *= 2;
+				if constexpr (IsReallocable_v<T>) {
+					buf = (T*)realloc((void*)buf, sizeof(T) * cap);
+				}
+				else {
+					T* newBuf = (T*)malloc(sizeof(T) * cap);
+					for (int i = 0; i < len; ++i) {
+						new(&newBuf[i]) T((T&&)buf[i]);
+						buf[i].~T();
+					}
+					free((void*)buf);
+					buf = newBuf;
+				}
+			}
+			idx = len;
+			++len;
+			return cap;
+		}
+	}
+
+	void Free(PNType const& idx) {
+		buf[idx].~T();
+		Next(buf[idx]) = header;
+		header = idx;
+		++count;
+	}
+
+	void Clear() {
+		for (int i = 0; i < len; ++i) {
+			buf[i].~T();
+		}
+		len = 0;
+		count = 0;
+		header = -1;
+	}
+
+	PNType Len() const {
+		return len;
+	}
+	PNType Count() const {
+		return len - count;
+	}
+
+	T& operator[](PNType const& idx) {
+		assert(idx >= 0 && idx < len);
+		return buf[idx];
+	}
+	T const& operator[](PNType const& idx) const {
+		assert(idx >= 0 && idx < len);
+		return buf[idx];
+	}
+};
--- structTest3/main.cpp
+++ structTest3/main.cpp
@@ -1,3 +1,158 @@
-﻿int main() {
+﻿// todo: ECS思考: 用组合来替代继承？类似列存储？数据按需求分组聚合？
+
+#include "LinkPool.h"
+#include <vector>
+#include <tuple>
+
+struct A {
+	int a;
+};
+struct B {
+	int b;
+};
+struct C {
+	int c;
+};
+
+template<typename T>
+struct W : T {
+	int prev;
+	int next;
+	int self;
+};
+
+template<typename T>
+using E = std::pair<LinkPool<W<T>>, int>;	// second: header
+
+std::tuple<E<A>, E<B>, E<C>> pool;
+
+template<class Tuple, std::size_t N>
+struct PoolIniter {
+	static void Init(Tuple& in) {
+		std::get<N - 1>(in).second = -1;
+		PoolIniter<Tuple, N - 1>::Init(in);
+	}
+};
+
+template<class Tuple>
+struct PoolIniter<Tuple, 0> {
+	static void Init(Tuple& in) {
+		std::get<0>(in).second = -1;
+	}
+};
+
+template<class Tuple>
+void PoolInit(Tuple& in) {
+	PoolIniter<Tuple, std::tuple_size<Tuple>::value>::Init(in);
+}
+
+struct StaticPoolIniter {
+	StaticPoolIniter() {
+		PoolInit(pool);
+	}
+} __StaticPoolIniter;
+
+template<typename T>
+struct Component {
+	int idx;
+	LinkPool<W<T>>& Pool() { return std::get<E<T>>(pool).first; }
+	int& Header() { return std::get<E<T>>(pool).second; }
+	W<T>& Node() { return Pool()[idx]; }
+	T& Data() { return (T&)Node(); }
+	operator T& () { return Data(); }
+	explicit Component() {
+		auto& p = Pool();
+		p.Alloc(idx);
+		auto& o = Node();
+		auto& h = Header();
+		o.prev = -1;
+		o.next = h;
+		if (h != -1) {
+			p[h].prev = idx;
+		}
+		h = idx;
+		//o.self = self;
+	}
+	~Component() {
+		auto& p = Pool();
+		auto& h = Header();
+		auto& o = Node();
+		if (o.prev != -1) {
+			p[o.prev].next = o.next;
+		}
+		if (o.next != -1) {
+			p[o.next].prev = o.prev;
+		}
+		if (h == idx) {
+			h = o.next;
+		}
+		p.Free(idx);
+	}
+};
+
+struct AB : Component<A>, Component<B> {
+	AB() {
+		// todo: get self's idx
+		this->Component<A>::Node().self = 0;
+		this->Component<B>::Node().self = 0;
+	}
+};
+
+int main() {
+	//AB ab;
+	//ab.Component<A>::i = 1;
 	return 0;
 }
+
+//template<size_t capA, size_t capB, size_t capC>
+//struct Env {
+//	Env() : as(capA), bs(capB), cs(capC) {}
+//	LinkPool<W<A>> as;
+//	int asHeader = -1;
+//	LinkPool<W<B>> bs;
+//	int bsHeader = -1;
+//	LinkPool<W<C>> cs;
+//	int csHeader = -1;
+//};
+//Env<10000, 10000, 10000> env;
+//
+//struct AB {
+//	int ia;
+//	A& a() { return env.as[ia]; }
+//	operator A&() { return a(); }
+//
+//	int ib;
+//	B& b() { return env.bs[ib]; }
+//	operator B& () { return b(); }
+//
+//	AB() {
+//		env.as.Alloc(ia);
+//		env.bs.Alloc(ib);
+//	}
+//	~AB() {
+//		env.as.Free(ia);
+//		env.bs.Free(ib);
+//	}
+//};
+//struct BC {
+//	int ib;
+//	B& b() { return env.bs[ib]; }
+//	operator B& () { return b(); }
+//
+//	int ic;
+//	C& c() { return env.cs[ic]; }
+//	operator C& () { return c(); }
+//
+//	BC() {
+//		env.bs.Alloc(ib);
+//		env.cs.Alloc(ic);
+//	}
+//	~BC() {
+//		env.bs.Free(ib);
+//		env.cs.Free(ic);
+//	}
+//};
+
+//struct World {
+//	std::vector<AB> abs;
+//};
