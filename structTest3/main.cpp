5fca282bdf6815ff4c4f8e8b0bf6e18a0e3b4ce6
denghe <denghe@hotmail.com>
2021-01-26 23:32:17 +0800

    refactor

--- structTest3/main.cpp
+++ structTest3/main.cpp
@@ -1,5 +1,4 @@
 ﻿#include "xx_combopool.h"
-
 #include <string>
 #include <iostream>
 #include <chrono>
@@ -17,14 +16,14 @@ struct C {
 	int hp;
 };
 
-// items
+// combos
 using ABC = xx::Combo<A, B, C>;
 using AB = xx::Combo<A, B>;
 using AC = xx::Combo<A, C>;
 using BC = xx::Combo<B, C>;
 
-using MyEnv = xx::ComboPool<xx::SlicesContainer<A, B, C>, xx::CombosContainer<ABC, AB, AC, BC>>;
-std::vector<MyEnv::Shared<ABC>> abcHolders;
+using CP = xx::ComboPool<xx::SlicesContainer<A, B, C>, xx::CombosContainer<ABC, AB, AC, BC>>;
+std::vector<CP::Shared<ABC>> abcHolders;
 
 struct Foo {
 	std::array<char, 512> dummy;
@@ -36,7 +35,7 @@ std::vector<Foo> foos;
 std::vector<std::shared_ptr<Foo>> fooPtrs;
 
 int main() {
-	MyEnv env;
+	CP cp;
 
 	for (size_t i = 0; i < 100000; i++) {
 		auto& f = foos.emplace_back();
@@ -47,14 +46,13 @@ int main() {
 
 		fooPtrs.emplace_back(std::make_shared<Foo>(f));
 
-		auto&& abc = env.CreateCombo<ABC>();
+		auto&& abc = cp.CreateCombo<ABC>();
 		abc.RefSlice<A>().name = f.name;
 		abc.RefSlice<B>().x = f.x;
 		abc.RefSlice<B>().y = f.y;
 		abc.RefSlice<C>().hp = f.hp;
 		abcHolders.push_back(std::move(abc));
 	}
-
 	uint64_t totalHP = 0;
 	auto tp = std::chrono::system_clock::now();
 	for (size_t i = 0; i < 10000; i++) { for (auto& f : foos) { totalHP += f.hp; } }
@@ -69,159 +67,9 @@ int main() {
 
 	totalHP = 0;
 	tp = std::chrono::system_clock::now();
-	for (size_t i = 0; i < 10000; i++) { env.ForeachSlices<C>([&](C& o, auto& owner) { totalHP += o.hp; }); }
+	for (size_t i = 0; i < 10000; i++) { cp.ForeachSlices<C>([&](C& o, auto& owner) { totalHP += o.hp; }); }
 	ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - tp).count();
-	std::cout << "env.ForeachSlices<C> ms = " << ms << ", totalHP = " << totalHP << std::endl;
+	std::cout << "cp.ForeachSlices<C> ms = " << ms << ", totalHP = " << totalHP << std::endl;
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-//
-//#include <chrono>
-//
-//// slices
-//struct A {
-//	SliceBaseCode(A);
-//	std::array<char, 512> dummy;
-//	std::string name;
-//};
-//struct B {
-//	float x, y;
-//};
-//struct C {
-//	int hp;
-//};
-//
-//// items
-//using ABC = O<A, B, C>;
-//using AB = O<A, B>;
-//using AC = O<A, C>;
-//using BC = O<B, C>;
-//
-//using MyEnv = Env<Vectors<A, B, C>, LinkPools<ABC, AB, AC, BC>>;
-//
-//int main() {
-//	MyEnv env;
-//	{
-//		std::vector<MyEnv::Shared<ABC>> abcHolders;
-//		abcHolders.reserve(5);
-//		abcHolders.push_back(env.CreateItem<ABC>());
-//		abcHolders.push_back(env.CreateItem<ABC>());
-//		std::cout << std::endl;
-//	}
-//	return 0;
-//}
-
-
-
-
-
-
-
-
-
-//
-//// slices
-//struct A {
-//	SliceBaseCode(A);
-//	std::string name;
-//};
-//struct B {
-//	float x, y;
-//};
-//struct C {
-//	int hp;
-//};
-//
-//// items
-//using ABC = O<A, B, C>;
-//using AB = O<A, B>;
-//using AC = O<A, C>;
-//using BC = O<B, C>;
-//
-//using MyEnv = Env<Vectors<A, B, C>, LinkPools<ABC, AB, AC, BC>>;
-//
-//std::array<char const*, 4> itemTypes{ "ABC", "AB", "AC", "BC" };
-//
-//int main() {
-//	MyEnv env;
-//
-//	auto&& abc = env.CreateItem<ABC>();
-//	abc.RefSlice<A>().name = "asdf";
-//	abc.RefSlice<B>().x = 1;
-//	abc.RefSlice<B>().y = 2;
-//	abc.RefSlice<C>().hp = 3;
-//
-//	// 出括号后自杀
-//	{
-//		auto&& bc = env.CreateItem<BC>();
-//		bc.RefSlice<B>().x = 1.2f;
-//		bc.RefSlice<B>().y = 3.4f;
-//		bc.RefSlice<C>().hp = 5;
-//	}
-//
-//	auto&& ac = env.CreateItem<AC>();
-//	ac.RefSlice<A>().name = "qwerrrr";
-//	ac.RefSlice<C>().hp = 7;
-//
-//	auto&& ac2 = env.CreateItem<AC>();
-//	ac2.RefSlice<A>().name = "e";
-//	ac2.RefSlice<C>().hp = 9;
-//
-//	env.ForeachSlices<A>([](A& o, auto& owner) {
-//		std::cout << "owner: " << itemTypes[owner.first] << ", idx = " << owner.second << std::endl;
-//		std::cout << "A::name = " << o.name << std::endl;
-//		});
-//	env.ForeachSlices<B>([](B& o, auto& owner) {
-//		std::cout << "owner: " << itemTypes[owner.first] << ", idx = " << owner.second << std::endl;
-//		std::cout << "B::x y = " << o.x << ", " << o.y << std::endl;
-//		});
-//	env.ForeachSlices<C>([](C& o, auto& owner) {
-//		std::cout << "owner: " << itemTypes[owner.first] << ", idx = " << owner.second << std::endl;
-//		std::cout << "C::hp = " << o.hp << std::endl;
-//		});
-//
-//	return 0;
-//}
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-//struct Bar {
-//	Bar() = default;
-//	Bar(Bar const&) {
-//		std::cout << "Bar const&" << std::endl;
-//	}
-//	Bar& operator=(Bar const&) {
-//		std::cout << "=Bar const&" << std::endl;
-//		return *this;
-//	}
-//	Bar(Bar&&) { 
-//		std::cout << "Bar&&" << std::endl;
-//	}
-//	Bar& operator=(Bar&&) { 
-//		std::cout << "=Bar&&" << std::endl;
-//		return *this;
-//	}
-//};
-//struct Foo { 
-//	SliceBaseCode(Foo)
-//	Bar bar;
-//};
