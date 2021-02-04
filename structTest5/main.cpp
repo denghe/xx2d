25e5c54afd7469c8fd2b7eabecc920cea41d8258
denghe <denghe@hotmail.com>
2021-02-04 18:51:24 +0800

    add some test result

--- structTest5/main.cpp
+++ structTest5/main.cpp
@@ -9,6 +9,8 @@ int main(int argc, char* argv[]) {
 
 	if (int r = wm.LoadFromXxmv("res/a.xxmv")) return r;
 	//if (int r = wm.SaveToPngs("res/", "a")) return r;
+
+	// 性能评测结果：下面这行代码 9900k cpu 大约执行 0.17x 秒. 如果不写磁盘 0.04x 秒. 手机上性能如何，需要实测。有可能慢 10 倍+ ?
 	if (int r = wm.SaveToPackedPngs("res/", "a")) return r;
 
 	std::cout << "done." << std::endl;
