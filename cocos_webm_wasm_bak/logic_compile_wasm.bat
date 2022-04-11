8cfa90b4be01d39816e0bbe8b3bc220adc8b3769
denghe <denghe@hotmail.com>
2022-04-11 12:45:29 +0800

    minor improve

--- cocos_webm_wasm_bak/logic_compile_wasm.bat
+++ cocos_webm_wasm_bak/logic_compile_wasm.bat
@@ -1,2 +1,1 @@
-em++ --no-entry -s ERROR_ON_UNDEFINED_SYMBOLS=0 -Wl,--export-dynamic -std=c++20 -O2 -I../../xxlib/src -I../../emsdk/upstream/emscripten  -DEMSCRIPTEN   Logic.cpp   -o ../Resources/logic.wasm
-#echo -s MALLOC=emmalloc 
+em++ --no-entry -s ERROR_ON_UNDEFINED_SYMBOLS=0 -std=c++20 -O2   Logic.cpp   -o ../Resources/logic.wasm
