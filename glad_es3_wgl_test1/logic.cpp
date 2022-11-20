332475f5967501aa5b99c4722c61dc50f7319298
denghe <denghe@hotmail.com>
2022-11-20 23:54:55 +0800

    writing...

--- glad_es3_wgl_test1/logic.cpp
+++ glad_es3_wgl_test1/logic.cpp
@@ -2,9 +2,50 @@
 #include "logic.h"
 
 Logic::Logic() {
-	v = LoadVertexShader({ R"()" });
-	f = LoadFragmentShader({ R"()" });
+	v = LoadVertexShader({ R"(#version 300 es
+precision highp float;
+uniform mat4 uMVPMatrix;
+
+in vec4 aPos;
+in vec2 aTexCoord;
+in vec4 aColor;
+
+out mediump vec4 vFragColor;
+out mediump vec2 vTexCoord;
+
+void main() {
+    gl_Position = uMVPMatrix * aPos;
+	vFragColor = aColor;
+	vTexCoord = aTexCoord;
+})" });
+	f = LoadFragmentShader({ R"(#version 300 es
+precision mediump float;
+uniform sampler2D uTex0;
+
+in vec4 vFragColor;
+in vec2 vTexCoord;
+
+out vec4 oColor;
+
+void main() {
+	oColor = vFragColor * texture(uTex0, vTexCoord);
+}
+)" });
 	p = LinkProgram(v, f);
+
+	glBindAttribLocation(p, 0, "aPos");
+	glBindAttribLocation(p, 1, "aTexCoord");
+	glBindAttribLocation(p, 2, "aColor");
+
+	uTex0 = glGetUniformLocation(p, "uTex0");
+	assert(uTex0 == 1);
+	glUniform1i(uTex0, 0);
+
+	uMVPMatrix = glGetUniformLocation(p, "uMVPMatrix");
+	assert(uMVPMatrix == 0);
+	//glUniform1i(uMVPMatrix, 0);
+
+	glUseProgram(p);
 }
 
 void Logic::Update() {
--- glad_es3_wgl_test1/logic.h
+++ glad_es3_wgl_test1/logic.h
@@ -5,6 +5,7 @@
 struct Logic {
 	Shader v, f;
 	Program p;
+	GLint uMVPMatrix = -1, uTex0 = -1;
 	VertexArrays va;
 	Buffer b1, b2;
 	std::vector<xx::Shared<Texture>> ts;
