5a523b35fe2007bc37ed0d8b1a7c8de815035d65
denghe <denghe@hotmail.com>
2022-12-30 11:48:28 +0800

    tiledmap reader writing...

---
+++ res/tiledmap1/tiles.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tiles" tilewidth="64" tileheight="64" spacing="2" margin="2" tilecount="48" columns="8">
+ <image source="tmw_desert_spacing-hd.png" width="530" height="398"/>
+ <wangsets>
+  <wangset name="desert ground" type="corner" tile="24">
+   <wangcolor name="sand" color="#ff0000" tile="29" probability="1"/>
+   <wangcolor name="cobblestone" color="#00ff00" tile="9" probability="1"/>
+   <wangcolor name="pavement" color="#0000ff" tile="33" probability="1"/>
+   <wangcolor name="dirt" color="#ff7700" tile="14" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="3" wangid="0,4,0,1,0,4,0,4"/>
+   <wangtile tileid="4" wangid="0,4,0,4,0,1,0,4"/>
+   <wangtile tileid="5" wangid="0,1,0,4,0,1,0,1"/>
+   <wangtile tileid="6" wangid="0,1,0,4,0,4,0,1"/>
+   <wangtile tileid="7" wangid="0,1,0,1,0,4,0,1"/>
+   <wangtile tileid="8" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="9" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="11" wangid="0,1,0,4,0,4,0,4"/>
+   <wangtile tileid="12" wangid="0,4,0,4,0,4,0,1"/>
+   <wangtile tileid="13" wangid="0,4,0,4,0,1,0,1"/>
+   <wangtile tileid="14" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="15" wangid="0,1,0,1,0,4,0,4"/>
+   <wangtile tileid="16" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="19" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="20" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="21" wangid="0,4,0,1,0,1,0,1"/>
+   <wangtile tileid="22" wangid="0,4,0,1,0,1,0,4"/>
+   <wangtile tileid="23" wangid="0,1,0,1,0,1,0,4"/>
+   <wangtile tileid="24" wangid="0,1,0,3,0,1,0,1"/>
+   <wangtile tileid="25" wangid="0,1,0,3,0,3,0,1"/>
+   <wangtile tileid="26" wangid="0,1,0,1,0,3,0,1"/>
+   <wangtile tileid="27" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="28" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="29" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="32" wangid="0,3,0,3,0,1,0,1"/>
+   <wangtile tileid="33" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,3,0,3"/>
+   <wangtile tileid="35" wangid="0,3,0,1,0,3,0,3"/>
+   <wangtile tileid="36" wangid="0,3,0,3,0,1,0,3"/>
+   <wangtile tileid="40" wangid="0,3,0,1,0,1,0,1"/>
+   <wangtile tileid="41" wangid="0,3,0,1,0,1,0,3"/>
+   <wangtile tileid="42" wangid="0,1,0,1,0,1,0,3"/>
+   <wangtile tileid="43" wangid="0,1,0,3,0,3,0,3"/>
+   <wangtile tileid="44" wangid="0,3,0,3,0,3,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
--- src/logic.cpp
+++ src/logic.cpp
@@ -1,116 +1,15 @@
 ﻿#include "pch.h"
 #include "logic.h"
-
+#include "tmxdata.h"
 
 void Logic::Init() {
-	//rnd.SetSeed();
-	fnt1 = LoadBMFont("res/font1/basechars.fnt"sv);
-	fnt2 = LoadBMFont("res/font2/basechars.fnt"sv);
-	LoadFramesFromCache(LoadTPData("res/zm.plist"sv));		// key = zazaka.pkm, mouse.pkm
-	fZazaka = frameCache.find("zazaka.pkm"sv)->second;
-	fMouse = frameCache.find("mouse.pkm"sv)->second;
-	lbCount.SetPositon(ninePoints[1] + XY{ 10, 10 });
-	lbCount.SetAnchor({0, 0});
-
-	scene.Emplace();
-	scene->Init(this, 100000);
+	coros.Add([](Logic* self)->CoType {
+		CoYield;
+	}(this));
 }
 
 int Logic::Update() {
 	if (Pressed(KbdKeys::Escape)) return 1;
-
-	scene->Update();
-
-	if (Pressed(Mbtns::Left)) {
-		for (int i = 0; i < 10; ++i) {
-			scene->Insert(mousePosition);
-		}
-	}
-	if (Pressed(Mbtns::Right)) {
-		//scene->Insert(mousePosition);
-		// todo: find & delete
-	}
-
-	for (auto& o : scene->objs) {
-		tmp.emplace_back(&o->spr);
-	}
-	std::sort(tmp.begin(), tmp.end(), [](Sprite* const& a, Sprite* const& b)->bool const { return a->pos.y > b->pos.y; });
-	for (auto&& o : tmp) {
-		o->Draw(this);
-	}
-	tmp.clear();
-
-	//for (auto& o : scene->objs) {
-	//	o.spr.Draw(this);
-	//}
-
-	lbCount.SetText(fnt1, xx::ToString("draw call = ", GetDrawCall(), ", quad count = ", GetDrawQuads()));
-	lbCount.Draw(this);
+	coros();
 	return 0;
 }
-
-
-
-
-//constexpr double fixedFrameDelta = 1.0 / 60;
-//timePool += lastDelta;
-//while (timePool >= fixedFrameDelta) {
-//	timePool -= fixedFrameDelta;
-//	coros();
-
-//	if (Pressed(Mbtns::Left)) {
-//		for (int i = 0; i < 100; ++i) {
-//			auto m = xx::Make<Monster>();
-//			auto&& [_, ok] = monsters.emplace(m);
-//			m->id = ++objsCounter;
-//			m->hp = 123;
-//			m->x = mousePosition.x;
-//			m->y = mousePosition.y;
-//			m->drawOrder = (double)m->y - m->id / 1000000000.;
-//			auto& s = m->sprite;
-//			auto& l = m->label;
-
-//			if (m->id % 2) {
-//				s.SetTexture(fZazaka);
-//				s.SetScale(rnd.Next(0.5f, 2.f));
-//			}
-//			else {
-//				s.SetTexture(fMouse);
-//				s.SetScale(rnd.Next(1.f, 4.f));
-//			}
-//			auto c = rnd.Get(); auto cp = (uint8_t*)&c;
-//			s.SetColor({ cp[0], cp[1], cp[2], 255 });
-//			s.SetPositon({ (float)m->x, (float)m->y });	// todo: mapping?
-
-//			l.SetText(fnt2, xx::ToString("id = ", m->id), 16);
-//			l.SetPositon({ s.pos.x, s.pos.y + s.frame->spriteSize.h * s.scale.y * (1 - s.anchor.y) });
-//			l.SetAnchor({ 0.5, 0 });
-//			l.scale = s.scale;
-
-//			coros.Add([](Logic* self, auto m)->CoType {
-//				CoYield;
-//				auto& s = m->sprite;
-//				auto& l = m->label;
-//				while (s.scale.x > 0.05) {
-//					s.SetScale(s.scale * 0.99);
-//					l.SetScale(s.scale);
-//					l.SetPositon({ s.pos.x, s.pos.y + s.frame->spriteSize.h * s.scale.y * (1 - s.anchor.y) });
-//					CoYield;
-//				}
-//				self->monsters.erase(m);
-//			}(this, m));
-//		}
-//	}
-//}
-
-//for (auto& o : monsters) {
-//	tmp.emplace_back(&*o);
-//}
-//std::sort(tmp.begin(), tmp.end(), [](auto const& a, auto const& b)->bool const { return a->drawOrder > b->drawOrder; });
-//for (auto&& o : tmp) {
-//	o->sprite.Draw(this);
-//}
-//for (auto&& o : tmp) {
-//	o->label.Draw(this);
-//}
-//tmp.clear();
