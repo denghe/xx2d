766e6ad0edbc3f78795cc5897ffcbc6600fcc839
denghe <denghe@hotmail.com>
2023-02-09 01:54:22 +0800

    writing...

--- src/logic8.cpp
+++ src/logic8.cpp
@@ -7,7 +7,18 @@ void Logic8::Init(Logic* logic) {
 
 	std::cout << "Logic8 Init( matrix tests )" << std::endl;
 
+	xx::Node n;
+	n.radians = M_PI / 2;
+	n.transform = xx::AffineTransform::Make(n.pos, n.anchorSize, n.scale, n.radians);
 
+	auto xy = n.transform.Apply({ 5, 0 });
+	std::cout << xy.x << std::endl;
+	std::cout << xy.y << std::endl;
+
+	//auto t1 = xx::AffineTransform::Rotate(xx::AffineTransform::MakeIdentity(), M_PI / 2.f);
+	//auto xy = t1.Apply({ 5, 0 });
+	//std::cout << xy.x << std::endl;
+	//std::cout << xy.y << std::endl;
 }
 
 int Logic8::Update() {
--- src/logic8.h
+++ src/logic8.h
@@ -9,34 +9,73 @@ namespace xx {
 		float a, b, c, d;
 		float tx, ty;
 
-		inline static AffineTransform Concat(AffineTransform const& t1, AffineTransform const& t2) {
-			return { t1.a * t2.a + t1.b * t2.c, 
-				t1.a * t2.b + t1.b * t2.d,
-				t1.c * t2.a + t1.d * t2.c, 
-				t1.c * t2.b + t1.d * t2.d,
-				t1.tx * t2.a + t1.ty * t2.c + t2.tx,
-				t1.tx * t2.b + t1.ty * t2.d + t2.ty };
+		inline static AffineTransform Make(XY const& pos, XY const& anchorSize, XY const& scale, float const& radians) {
+			auto x = pos.x;
+			auto y = pos.y;
+			float cx = 1, sx = 0, cy = 1, sy = 0;
+			if (radians) {
+				auto c = std::cos(-radians);
+				auto s = std::sin(-radians);
+				cx = c * scale.x;
+				cy = c * scale.y;
+				sx = -s * scale.x;
+				sy = s * scale.y;
+			}
+			if (!anchorSize.IsZero()) {
+				x += cy * -anchorSize.x + sx * -anchorSize.y;
+				y += sy * -anchorSize.x + cx * -anchorSize.y;
+			}
+			return { cy, sy, sx, cx, x, y };
 		}
 
-		inline static AffineTransform Translate(AffineTransform const& t, float tx, float ty) {
-			return { t.a, t.b, t.c, t.d, t.tx + t.a * tx + t.c * ty, t.ty + t.b * tx + t.d * ty };
+		inline static AffineTransform MakeIdentity() {
+			return { 1.0, 0.0, 0.0, 1.0, 0.0, 0.0 };
 		}
 
-		inline static AffineTransform CCAffineTransformScale(const AffineTransform& t, float sx, float sy) {
-			return { t.a * sx, t.b * sx, t.c * sy, t.d * sy, t.tx, t.ty };
+		inline AffineTransform MakeConcat(AffineTransform const& t) const {
+			return { a * t.a + b * t.c, 
+				a * t.b + b * t.d,
+				c * t.a + d * t.c, 
+				c * t.b + d * t.d,
+				tx * t.a + ty * t.c + t.tx,
+				tx * t.b + ty * t.d + t.ty };
 		}
 
-		inline static AffineTransform CCAffineTransformRotate(const AffineTransform& t, float radians) {
-			float fSin = sinf(radians);
-			float fCos = cosf(radians);
-			return { t.a * fCos + t.c * fSin,
-				t.b * fCos + t.d * fSin,
-				t.c * fCos - t.a * fSin,
-				t.d * fCos - t.b * fSin,
-				t.tx, t.ty };
+		inline AffineTransform MakeInvert(AffineTransform const& t) {
+			float de = 1 / (t.a * t.d - t.b * t.c);
+			return { de * t.d, -de * t.b, -de * t.c, de * t.a,
+				de * (t.c * t.ty - t.d * t.tx), de * (t.b * t.tx - t.a * t.ty) };
 		}
 
-		XY Apply(XY const& point) const {
+		inline AffineTransform& Translate(XY const& pos) {
+			tx += a * pos.x + c * pos.y;
+			ty += b * pos.x + d * pos.y;
+			return *this;
+		}
+
+		inline AffineTransform& Scale(XY const& scale) {
+			a *= scale.x;
+			b *= scale.x;
+			c *= scale.y;
+			d *= scale.y;
+			return *this;
+		}
+
+		inline AffineTransform& Rotate(float const& radians) {
+			auto fSin = std::sin(radians);
+			auto fCos = std::cos(radians);
+			auto ta = a * fCos + c * fSin;
+			auto tb = b * fCos + d * fSin;
+			auto tc = c * fCos - a * fSin;
+			auto td = d * fCos - b * fSin;
+			a = ta;
+			b = tb;
+			c = tc;
+			d = td;
+			return *this;
+		}
+
+		inline XY Apply(XY const& point) const {
 			return { (float)((double)a * point.x + (double)c * point.y + tx),
 			(float)((double)b * point.x + (double)d * point.y + ty) };
 		}
@@ -44,33 +83,9 @@ namespace xx {
 
 	struct Node {
 		AffineTransform transform;
-		XY pos{}, anchor{}, anchorSize{}, size{}, scale{};
+		XY pos{}, anchor{}, anchorSize{}, size{}, scale{ 1, 1 };
 		float radians{};
-
-		void FillTransform() {
-			auto x = pos.x;
-			auto y = pos.y;
-
-			float cx = 1, sx = 0, cy = 1, sy = 0;
-
-			if (radians) {
-				auto c = cosf(-radians);
-				auto s = sinf(-radians);
-				cx = c * scale.x;
-				cy = c * scale.y;
-				sx = -s * scale.x;
-				sy = s * scale.y;
-			}
-
-			if (!anchorSize.IsZero()) {
-				x += cy * -anchorSize.x + sx * -anchorSize.y;
-				y += sy * -anchorSize.x + cx * -anchorSize.y;
-			}
-
-			transform = { cy, sy, sx, cx, x, y };
-		}
 	};
-
 }
 
 
