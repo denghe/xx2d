5d65a570ff78fed8b50e6d32091cdfdc35db3689
denghe <denghe@hotmail.com>
2023-05-11 17:59:57 +0800

    writing...

--- res/test1.lua
+++ res/test1.lua
@@ -1,3 +1,217 @@
+-- 测试结果， 这样写就有一千多 fps, 协程写法只有 2 fps
+
+gTimePool = 0
+gDelta = 0.0166666666666667
+
+-- 被 cpp 每帧调用
+function Update(delta)
+	local tp = gTimePool
+	tp = tp + delta
+	while tp >= 0.0166666666666667 do
+		tp = tp - 0.0166666666666667
+		Logic()
+	end
+	gTimePool = tp
+
+	if Draw then
+		Draw()
+	end
+end
+
+
+local gQuads = {}
+local tex = xxLoadSharedTexture("res/tree.pkm")
+
+for j = 1, 10000 do
+	
+	local ctx = {}
+	ctx[1] = xxQuad():SetTexture( tex ):SetPosition( xxRndNext( -500, 500 ), xxRndNext( -300, 300 ) )
+	ctx[2] = 0
+	ctx[3] = xxRndNext( 100, 300 )
+	gQuads[j] = ctx
+
+end
+
+Logic = function()
+
+	local t = gQuads
+	local n = #t
+	if n == 0 then return end
+	for i = n, 1, -1 do
+		local c = t[i]
+		c[1]:AddRotate(gDelta)
+		c[2] = c[2] + 1
+		if c[2] >= c[3] then
+			table.remove(t, i)
+		end
+	end
+
+end
+
+Draw = function()
+	for j, c in pairs(gQuads) do
+		c[1]:Draw()
+	end
+end
+
+
+
+--[[
+
+-- 为了便于使用
+yield = coroutine.yield																			
+
+__G__TRACKBACK__ = function(msg)
+    local msg = debug.traceback(msg, 3)
+    print(msg)
+    return msg
+end
+
+-- 全局协程池( 经测试发现，lua 协程 resume 开销非常巨大， 一万个就会降到 2 fps. 所以在需要性能的逻辑里就不要上协程了 )
+local gCoros = {}																				
+
+-- 压入一个协程函数. 有参数就跟在后面. 有延迟执行的效果. 报错时带 name 显示
+-- 参数传入  func + args...  或  name + func + args...   或  ctx{ name, ... } + func + args...
+go = function(...)
+	local args = {...}
+	local t1 = type(args[1])
+	local t2 = type(args[2])
+	assert( #args > 0 and ( t1 == 'function' or t2 == 'function') )
+
+	local ctx
+	local func
+	if t1 ~= 'function' then
+		if t1 == 'string' then
+			ctx = { name = args[1] }
+		else
+			ctx = args[1]
+		end
+		func = args[2]
+		table.remove(args, 1)
+	else
+		ctx = { name = "" }
+		func = args[1]
+	end
+	table.remove(args, 1)
+
+	ctx[1] = coroutine.create(function() xpcall(func, __G__TRACKBACK__, ctx) end)
+	table.insert(gCoros, ctx)
+
+	return ctx
+end
+
+--- 通过名称获取协程上下文
+---@param name  string
+---@return table
+goContext = function(name)
+	for k, v in pairs(gCoros) do
+		if v.name == name then
+			return v
+		end
+	end
+end
+
+-- 睡指定秒( 保底睡3帧 )( coro )
+SleepSecs = function(secs)
+	local timeout = NowEpochMS() + secs * 1000
+	yield()
+	yield()
+	yield()
+	while timeout > NowEpochMS() do 
+		yield()
+	end
+end
+
+-- 固定帧率被 Update 调用, 执行所有 coros
+GlobalUpdate = function()
+	local cr = coroutine.resume
+
+	if gCoro then
+		if not cr(gCoro) then
+			gCoro = nil
+		end
+	end
+
+	local t = gCoros
+	local n = #t
+	if n == 0 then return end
+	for i = n, 1, -1 do
+		if not cr(t[i][1]) then
+			table.remove(t, i)
+		end
+	end
+end
+
+gTimePool = 0
+gDelta = 0.0166666666666667
+
+-- 被 cpp 每帧调用
+function Update(delta)
+	local tp = gTimePool
+	tp = tp + delta
+	while tp >= 0.0166666666666667 do
+		tp = tp - 0.0166666666666667
+		GlobalUpdate()
+	end
+	gTimePool = tp
+
+	if Draw then
+		Draw()
+	end
+end
+
+
+local gQuads = {}
+local tex = xxLoadSharedTexture("res/tree.pkm")
+
+for j = 1, 10000 do
+	
+	go("coro "..j, function(c)
+		--print( c.name .. " begin" )
+		local q = xxQuad():SetTexture( tex ):SetPosition( xxRndNext( -500, 500 ), xxRndNext( -300, 300 ) )
+		gQuads[q] = 1
+		for i = 1, xxRndNext( 1000, 2000 ) do
+			q:AddRotate(gDelta)
+			yield()
+		end
+		gQuads[q] = nil
+		--print( c.name .. " end" )
+	end)
+
+end
+
+Draw = function()
+	for o, _ in pairs(gQuads) do
+		o:Draw()
+	end
+end
+
+]]
+
+
+
+
+
+
+
+--[[
+
+
+local tex = xxLoadSharedTexture("res/tree.pkm")
+
+local q = xxQuad()
+q:SetTexture( tex )
+
+function FixedUpdate(delta)
+	q:AddRotate(delta)
+end
+
+function Update(delta)
+	q:Draw()
+end
+
+
+-- test user value for userdata
 
 function GetUserValue( ud )
 	local t = debug.getuservalue( ud )
@@ -8,11 +222,11 @@ function GetUserValue( ud )
 	return t
 end
 
-local t = xxLoadSharedTexture("res/sword.png")
+local t = xxLoadSharedTexture("res/tree.pkm")
 print(t)
 
 local q = xxQuad()
-q:SetTexture( t ):SetScale( 0.5 )
+q:SetTexture( t )
 print(q)
 print(t == q:GetTexture())
 
@@ -21,11 +235,5 @@ t.abc = 123
 local t2 = GetUserValue(q)
 print(t2.abc)
 
+]]
 
-function FixedUpdate(delta)
-	q:AddRotate(delta)
-end
-
-function Update(delta)
-	q:Draw()
-end
--- tests/main.cpp
+++ tests/main.cpp
@@ -516,6 +516,7 @@ namespace xx::Lua {
 namespace xx::Lua::Engine {
 
 	inline void Register(lua_State* const& L) {
+		xx::Lua::Data::Register(L);
 		xx::Lua::Quad::Register(L);
 		// ...
 
@@ -525,9 +526,85 @@ namespace xx::Lua::Engine {
 			return 0;
 			});
 
+		SetGlobalCClosure(L, "xxSearchPathReset", [](auto L)->int {
+			xx::engine.SearchPathReset();
+			return 0;
+			});
+
+		SetGlobalCClosure(L, "xxGetFullPath", [](auto L)->int {
+			auto fn = To<std::string_view>(L);
+			if (lua_gettop(L) > 1) {
+				auto fnIsFileName = To<bool>(L, 2);
+				return Push(L, xx::engine.GetFullPath(fn, fnIsFileName));
+			} else {
+				return Push(L, xx::engine.GetFullPath(fn));
+			}
+			return 0;
+			});
+
+		SetGlobalCClosure(L, "xxLoadFileDataWithFullPath", [](auto L)->int {
+			auto fp = To<std::string_view>(L);
+			if (lua_gettop(L) > 1) {
+				auto autoDecompress = To<bool>(L, 2);
+				return Push(L, xx::engine.LoadFileDataWithFullPath(fp, autoDecompress));
+			} else {
+				return Push(L, xx::engine.LoadFileDataWithFullPath(fp));
+			}
+			return 0;
+			});
+
+		SetGlobalCClosure(L, "LoadFileData", [](auto L)->int {
+			auto fn = To<std::string_view>(L);
+			if (lua_gettop(L) > 1) {
+				auto autoDecompress = To<bool>(L, 2);
+				return Push(L, xx::engine.LoadFileData(fn, autoDecompress));
+			} else {
+				return Push(L, xx::engine.LoadFileData(fn));
+			}
+			return 0;
+			});
+
+		SetGlobalCClosure(L, "DetectFileFormat", [](auto L)->int {
+			auto d = To<xx::Data*>(L);
+			return Push(L, xx::engine.DetectFileFormat(*d));
+			});
+
+		// ...
+		//BMFont LoadBMFont(std::string_view const& fn);
+
+		// ...
+
 		SetGlobalCClosure(L, "xxLoadSharedTexture", [](auto L)->int {
 			auto fn = To<std::string_view>(L);
 			return Push(L, xx::engine.LoadSharedTexture(fn));
+			});
+
+		// ...
+
+		SetGlobalCClosure(L, "xxRndNextFloat", [](auto L)->int {
+			switch (lua_gettop(L)) {
+			case 0:
+				return Push(L, xx::engine.rnd.Next<float>());
+			case 1:
+				return Push(L, xx::engine.rnd.Next<float>(0, To<float>(L, 1)));
+			case 2:
+				return Push(L, xx::engine.rnd.Next<float>(To<float>(L, 1), To<float>(L, 2)));
+			default:
+				return 0;
+			}
+			});
+
+		SetGlobalCClosure(L, "xxRndNext", [](auto L)->int {
+			switch (lua_gettop(L)) {
+			case 0:
+				return Push(L, xx::engine.rnd.Next());
+			case 1:
+				return Push(L, xx::engine.rnd.Next(0, To<int32_t>(L, 1)));
+			case 2:
+				return Push(L, xx::engine.rnd.Next(To<int32_t>(L, 1), To<int32_t>(L, 2)));
+			default:
+				return 0;
+			}
 			});
 
 		// ...
@@ -546,6 +623,7 @@ void GameLooper::Init() {
 	font3500 = xx::engine.LoadBMFont("res/font/3500+.fnt"sv);
 	fpsViewer.Init(fontBase);
 
+	xx::Lua::RegisterBaseEnv(L);
 	xx::Lua::Engine::Register(L);
 
 	auto [data, fullpath] = xx::engine.LoadFileData("res/test1.lua");
@@ -554,12 +632,15 @@ void GameLooper::Init() {
 }
 
 int GameLooper::Update() {
-	timePool += xx::engine.delta;
-	while (timePool >= 1.f / 60) {
-		timePool -= 1.f / 60;
-		xL::CallGlobalFunc(L, "FixedUpdate", 1.f / 60);
-	}
+	//xL::CallGlobalFunc(L, "GlobalUpdate", xx::engine.delta);
+
+	//timePool += xx::engine.delta;
+	//while (timePool >= 1.f / 60) {
+	//	timePool -= 1.f / 60;
+	//	xL::CallGlobalFunc(L, "FixedUpdate", 1.f / 60);
+	//}
 	xL::CallGlobalFunc(L, "Update", xx::engine.delta);
+
 	fpsViewer.Update();
 	return 0;
 }
