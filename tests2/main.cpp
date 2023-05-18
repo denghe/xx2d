b241a5e628c83fbef847464ce0452f137048c2a6
denghe <denghe@hotmail.com>
2023-05-18 19:00:01 +0800

    writing...

--- tests2/main.cpp
+++ tests2/main.cpp
@@ -21,6 +21,27 @@ int GameLooper::Update() {
 	return 0;
 }
 
+
+
+struct A {
+	int64_t typeId = 0;
+};
+
+struct B {
+	int64_t typeId = 1;
+	bool b1{true};
+};
+
+struct C1 : B {
+	bool b2{};
+};
+
+struct C2 {
+	int64_t typeId = 2;
+	bool b1{ true };
+	bool b2{};
+};
+
 struct Foo {
 	int typeId = 1;
 	~Foo() {
@@ -35,11 +56,12 @@ struct Bar {
 };
 
 int main() {
-	xx::Shared<int> c1, c2;
-	c1.Emplace<Foo>();
-	c2.Emplace<Bar>();
-	std::cout << *c1 << std::endl;
-	std::cout << *c2 << std::endl;
+	xx::Shared<A> c1, c2;
+	c1.Emplace<C1>();
+	c2.Emplace<C2>();
+
+	std::cout << c1->typeId << std::endl;
+	std::cout << c2->typeId << std::endl;
 	return 0;
 
 
