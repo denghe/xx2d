c9aca783d755fb3c7e7aeedb2f454ca9383d085d
denghe <denghe@hotmail.com>
2023-09-03 12:00:43 +0800

    add some test code

--- tools/plist2blist/main.cpp
+++ tools/plist2blist/main.cpp
@@ -15,11 +15,13 @@ int main() {
 
 		xx::TP tp;
 		tp.Load((std::string&)p.u8string(), true, false);
+		std::cout << "handle file: " << p << std::endl;
 		xx::Data d;
 		d.Write(tp.premultiplyAlpha);
 		d.Write(tp.realTextureFileName);
 		d.Write(tp.frames.size());
 		for (auto& f : tp.frames) {
+			std::cout << "handle frame: " << f->key << std::endl;
 			d.Write(f->key);
 			d.Write(f->anchor);
 			d.Write((xx::RWFloatUInt16&)f->spriteOffset.x, (xx::RWFloatUInt16&)f->spriteOffset.y);
@@ -30,6 +32,23 @@ int main() {
 		}
 		xx::WriteAllBytes(newPath, d);
 		++n;
+
+		//bool premultiplyAlpha;
+		//std::string realTextureFileName;
+		//size_t numFrames{};
+		//if (int r = d.Read(premultiplyAlpha, realTextureFileName, numFrames)) return r;
+		//for (size_t i = 0; i < numFrames; ++i) {
+		//	std::string frameKey;
+		//	std::optional<xx::XY> anchor;
+		//	xx::XY spriteOffset, spriteSize, spriteSourceSize;
+		//	float x, y, w, h;
+		//	if (int r = d.Read(frameKey, anchor)) return r;
+		//	if (int r = d.Read((xx::RWFloatUInt16&)spriteOffset.x, (xx::RWFloatUInt16&)spriteOffset.y)) return r;
+		//	if (int r = d.Read((xx::RWFloatUInt16&)spriteSize.x, (xx::RWFloatUInt16&)spriteSize.y)) return r;
+		//	if (int r = d.Read((xx::RWFloatUInt16&)spriteSourceSize.x, (xx::RWFloatUInt16&)spriteSourceSize.y)) return r;
+		//	if (int r = d.Read((xx::RWFloatUInt16&)x, (xx::RWFloatUInt16&)y)) return r;
+		//	if (int r = d.Read((xx::RWFloatUInt16&)w, (xx::RWFloatUInt16&)h)) return r;
+		//}
 	}
 	std::cout << "finished. handled " << n << " files! \npress any key continue...\n";
 	std::cin.get();
